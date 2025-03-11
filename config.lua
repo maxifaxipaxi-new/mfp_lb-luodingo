1dbb36c5062c69a0c4e90facb3dcdc17d74c881e
MaxiFaxiPaxi <115405418+maxifaxipaxi-new@users.noreply.github.com>
2025-03-11 14:36:13 +0100

    Add files via upload

--- config.lua
+++ config.lua
@@ -1,10 +1,10 @@
 Config = {}
 
-Config.name = "Am I Fat - PRO" -- Name of the App
-Config.description = "Check your BMI Daily" -- disc in the appstore (edit for your local)
+Config.name = "Luodingo" -- Name of the App
+Config.description = "Learn or Luo will find you..." -- disc in the appstore (edit for your local)
 Config.defaultApp = false -- OPTIONAL if set to true, app should be added without having to download it,
-Config.size = 6932 -- OPTIONAL in kb
+Config.size = 26932 -- OPTIONAL in kb
 Config.price = 0 -- OPTIONAL, Make players pay with in-game money to download the app
 
 
--- Support on discord.mfpscripts.com if needed. Check out issues on github. --
+-- Check out https://mfp.tebex.io for more. --
